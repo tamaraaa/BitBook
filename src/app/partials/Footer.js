ca6e0e4457e1810aefdc8e7bdeb636fb1085fe7e
BIT Student <classrom@bgit.rs>
2018-03-27 14:28:00 +0200

    task1

---
+++ src/app/partials/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+
+export const Footer = () => {
+    return (
+        <footer class="page-footer container">
+            <div class="footer-copyright">
+                <div class="container">
+                    © 2014 Copyright Text
+
+                </div>
+            </div>
+        </footer>
+    )
+}
